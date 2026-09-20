33452b303b46fb178acd5461680b485e9494cb3d
Devangana T S <148966769+Thakkudts@users.noreply.github.com>
2026-09-20 15:21:34 +0530

    Add loan approval prediction app using Streamlit

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("loan_approval_decision_tree.pkl")
+
+st.title("Loan Approval Prediction")
+st.subheader("Decision Tree")
+
+income = st.number_input(
+    "Income",
+    min_value=0,
+    value=50000,
+    step=1000
+)
+
+credit_score = st.number_input(
+    "Credit Score",
+    min_value=0,
+    max_value=900,
+    value=720,
+    step=10
+)
+
+
+if st.button("Predict Loan Approval"):
+
+    new_applicant = pd.DataFrame({
+        "Income": [income],
+        "Credit_Score": [credit_score]
+    })
+
+    prediction = model.predict(new_applicant)
+
+    if prediction[0] == "YES":
+        st.success("Prediction: LOAN APPROVED")
+    else:
+        st.error("Prediction: LOAN NOT APPROVED")
